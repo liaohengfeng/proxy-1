021714b8b83e19c3b91c4448d6e25bca94c898f7
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-11-13 14:42:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'vercel.com'
+      url.hostname = 'github-readme-stats.vercel.app'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
